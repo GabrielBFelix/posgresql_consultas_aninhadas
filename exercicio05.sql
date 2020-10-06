db47ab25b788c249a1e60415e91678f7062ab9a5
GabrielBFelix <batistagabriel360@gmail.com>
2020-10-06 19:17:01 -0300

    adicionando exercicio06

---
+++ exercicio05.sql
@@ -0,0 +1,5 @@
+-- a) Explique os problemas de termos valores nulos nos dados.
+-- b) Explique o funcionamento do Right e do Left Join.
+-- c) Explique o funcionamento do Full Outer Join e como pode ser feito no MySQL ou MariaDB que não tem mais o comando Full.
+
+
